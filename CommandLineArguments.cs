de63776fce1bedf77c293dfd4f8fb42915459184
Matthew Olivo <saviorxtanren@gmail.com>
2020-12-04 00:01:25 -0800

    - Updating help text for command line arguments

--- CommandLineArguments.cs
+++ CommandLineArguments.cs
@@ -6,41 +6,41 @@ namespace SpeedRunDotCom_Console
 {
     public class CommandLineArguments
     {
-        [Option('g', "Game", Required = true, MetaValue = "GAME NAME")]
+        [Option('g', "Game", Required = true, MetaValue = "GAME NAME", HelpText = "The name of the game.")]
         public IEnumerable<string> GameNameList { get; set; }
 
-        [Option('c', "Category", Required = true, Default = null, MetaValue = "ANY%/100%/etc..", HelpText = "The specific category for the game")]
+        [Option('c', "Category", Required = true, Default = null, MetaValue = "ANY%/100%/etc..", HelpText = "The specific category for the game.")]
         public IEnumerable<string> CategoryList { get; set; }
 
-        [Option('o', "OutputToFile", HelpText = "Output results to \"output.txt\" file in executing location")]
-        public bool OutputToFile { get; set; }
-
-        [Option('p', "Platform", Required = false, Default = null, MetaValue = "PC/PS4/etc..", HelpText = "The specific platform for the game")]
-        public IEnumerable<string> PlatformList { get; set; }
-
-        [Option("f1", Default = null, HelpText = "The 1st name of a variable to filter on")]
-        public IEnumerable<string> Filter1NamesList { get; set; }
-
-        [Option("v1", Default = null, HelpText = "The 1st value of a variable to filter on")]
-        public IEnumerable<string> Filter1ValuesList { get; set; }
-
-        [Option("f2", Default = null, HelpText = "The 2nd name of a variable to filter on")]
-        public IEnumerable<string> Filter2NamesList { get; set; }
-
-        [Option("v2", Default = null, HelpText = "The 2nd value of a variable to filter on")]
-        public IEnumerable<string> Filter2ValuesList { get; set; }
-
-        [Option("f3", Default = null, HelpText = "The 3rd name of a variable to filter on")]
-        public IEnumerable<string> Filter3NamesList { get; set; }
-
-        [Option("v3", Default = null, HelpText = "The 3rd value of a variable to filter on")]
-        public IEnumerable<string> Filter3ValuesList { get; set; }
-
-        [Option('w', "WorldRecord", Group = "SearchType", HelpText = "Whether to show the world record time or not")]
+        [Option('w', "WorldRecord", Group = "SearchType", HelpText = "Whether to show the world record time or not. The -w or -u arguments is required.")]
         public bool WorldRecord { get; set; }
 
-        [Option('u', "Username", Group = "SearchType", MetaValue = "USERNAME", HelpText = "Username to look up record for")]
+        [Option('u', "Username", Group = "SearchType", MetaValue = "USERNAME", HelpText = "Username to look up record for. The -w or -u arguments is required.")]
         public string Username { get; set; }
+
+        [Option('p', "Platform", Required = false, Default = null, MetaValue = "PC/PS4/etc..", HelpText = "The specific platform for the game.")]
+        public IEnumerable<string> PlatformList { get; set; }
+
+        [Option("f1", Default = null, HelpText = "The 1st name of a variable to filter on.")]
+        public IEnumerable<string> Filter1NamesList { get; set; }
+
+        [Option("v1", Default = null, HelpText = "The 1st value of a variable to filter on.")]
+        public IEnumerable<string> Filter1ValuesList { get; set; }
+
+        [Option("f2", Default = null, HelpText = "The 2nd name of a variable to filter on.")]
+        public IEnumerable<string> Filter2NamesList { get; set; }
+
+        [Option("v2", Default = null, HelpText = "The 2nd value of a variable to filter on.")]
+        public IEnumerable<string> Filter2ValuesList { get; set; }
+
+        [Option("f3", Default = null, HelpText = "The 3rd name of a variable to filter on.")]
+        public IEnumerable<string> Filter3NamesList { get; set; }
+
+        [Option("v3", Default = null, HelpText = "The 3rd value of a variable to filter on.")]
+        public IEnumerable<string> Filter3ValuesList { get; set; }
+
+        [Option('o', "OutputToFile", HelpText = "Output results to \"output.txt\" file in executing location.")]
+        public bool OutputToFile { get; set; }
 
         public string GameName { get { return string.Join(" ", this.GameNameList); } }
 
